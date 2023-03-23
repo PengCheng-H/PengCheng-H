a04478355c344d863fb1b2cac8f7efdb6842f1c6
Pengcheng He <pengcheng.he@hcrobots.com>
2023-03-24 00:41:36 +0800

    stop working

--- src/pages/Workbench/pick_station/index.tsx
+++ src/pages/Workbench/pick_station/index.tsx
@@ -13,7 +13,7 @@ export default class HCPickStation extends React.Component {
 
     render(): React.ReactNode {
         return <div className="hc_panel">
-            <Button type="default" onClick={this.GetPickStations.bind(this)} style={{ margin: "5px" }}>更新拣货台状态</Button>
+            <Button type="primary" onClick={this.GetPickStations.bind(this)} style={{ margin: "5px" }}>更新拣货台状态</Button>
             <Button type="primary" onClick={this.StartWork.bind(this)} style={{ margin: "5px" }}>开始作业</Button>
             <Button type="primary" onClick={this.StopWork.bind(this)} style={{ margin: "5px" }}>停止作业</Button>
             <Table columns={pick_station_headers} dataSource={this.state.pick_station_list} pagination={{ pageSize: 10 }} className='table' />
@@ -43,22 +43,23 @@ export default class HCPickStation extends React.Component {
     }
 
     async StartWork() {
-        const activate_result: IHttpRes.IHCResponse = await api.WorkbenchStartWork();
-        if (!activate_result || activate_result.result_code == 0) {
-            message.error("开始作业失败！");
+        const start_result: IHttpRes.IHCResponse = await api.WorkbenchStartWork();
+        if (!start_result || start_result.result_code !== 0) {
+            message.error(`开始作业失败！result_code: ${start_result.result_code} err_msg: ${start_result.result_msg}`);
             return;
         }
         message.success("开始作业成功。");
+        setTimeout(() => { window.location.reload() }, 1000);
     }
 
     async StopWork() {
-        const activate_result: IHttpRes.IHCResponse = await api.WorkbenchStartWork();
-        if (!activate_result || activate_result.result_code == 0) {
-            message.error("停止作业失败！");
+        const stop_result: IHttpRes.IHCResponse = await api.WorkbenchStopWork();
+        if (!stop_result || stop_result.result_code !== 0) {
+            message.error(`停止作业失败！result_code: ${stop_result.result_code} err_msg: ${stop_result.result_msg}`);
             return;
         }
         message.success("停止作业成功。");
-
+        setTimeout(() => { window.location.reload() }, 1000);
     }
 }
 
