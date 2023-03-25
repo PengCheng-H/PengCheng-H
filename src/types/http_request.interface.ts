91252dc491ca61115000cbdd199cc98d9453f1e7
Pengcheng He <pengcheng.he@hcrobots.com>
2023-03-25 12:29:11 +0800

    手工分配料箱

--- src/types/http_request.interface.ts
+++ src/types/http_request.interface.ts
@@ -1,3 +1,23 @@
-export interface IHCRequest {
+import * as IBase from "./interface";
 
+export interface IHCRequest { }
+
+export interface IHCInventoryFindEmptyBoxesReq extends IHCRequest {
+    box_code?: string;
+    is_allocated?: boolean;
+    include_item_code?: string;
+    rows?: number[];
+    columns?: number[];
+    layers?: number[];
+    pagination_param?: IBase.IHCPagination;
+}
+
+// OrderInboundManualAllocateDetails
+export interface IHCOrderInboundManaulAllocateDetailsReq extends IHCRequest {
+    order_code: string,
+    order_details: {
+        order_detail_id: number,
+        box_code: string,
+        allocate_quantity: number
+    }[]
 }
