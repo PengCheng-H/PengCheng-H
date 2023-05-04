3d45ac8c753f44b042c09d34bf1189d4dec7f2d2
PengCheng <pengcheng.he@hcrobots.com>
2023-05-04 10:21:27 +0800

    基础

--- 2_react_ts_vite/src/main.tsx
+++ 2_react_ts_vite/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import zhCN from 'antd/locale/zh_CN';
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd';
+
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
     <App />
-  </React.StrictMode>,
+  </ConfigProvider>,
 )
